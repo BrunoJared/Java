019ae1643b8fbcb0e925002f2057ba98380123cf
BrunoJared <52966694+BrunoJared@users.noreply.github.com>
2020-07-20 23:47:50 -0300

    raio quatro casas decimais pos virgula

--- exercicioImpressão/src/exercicioImpressão/RaioCirculo.java
+++ exercicioImpressão/src/exercicioImpressão/RaioCirculo.java
@@ -7,22 +7,25 @@ public class RaioCirculo {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		float a1, a2, a3, areaA, areaB, areaC, y, x;
+		float a1, a2, a3, areaA, areaB, areaC, y, pi;
 		
-		x = (float) 3.14159;
+		pi = (float) 3.14159;
 		y = 2;
 		
 		a1 = sc.nextFloat();
 		a2 = sc.nextFloat();
 		a3 = sc.nextFloat();
 		
-		areaA = (float) (x * Math.pow(a1, y));
-		areaB = (float) (x * Math.pow(a2, y));
-		areaC = (float) (x * Math.pow(a3, y));
+		areaA = (float) (pi * Math.pow(a1, y));
+		areaB = (float) (pi * Math.pow(a2, y));
+		areaC = (float) (pi * Math.pow(a3, y));
 		
-		System.out.println("Area ="+ areaA );
-		System.out.println("Area ="+ areaB );
-		System.out.println("Area ="+ areaC );
+		System.out.printf("A=%.4f%n", areaA);
+		//System.out.println("Area ="+ areaA );
+		System.out.printf("A=%.4f%n", areaB);
+		//System.out.println("Area ="+ areaB );
+		System.out.printf("A=%.4f%n", areaC);
+		//System.out.println("Area ="+ areaC );
 		
 		sc.close();
 		
